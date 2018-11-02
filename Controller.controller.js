fae22691c56c5abfe9c7d1f1ee074bbb18f2fb32
S0013963210 <erwin.degula@fonterra.com>
2018-11-02 19:47:06 +0000

    Cleaned up codes

--- Controller.controller.js
+++ Controller.controller.js
@@ -12,55 +12,6 @@ sap.ui.define(
 
 	var ControllerController = Controller.extend("sap.ui.unified.sample.FileUploaderBasic.Controller", {
 		
-		onInit : function () {
-			// set explored app's demo model on this sample
-			var oJSONModel = this.initSampleDataModel();
-			this.getView().byId("tableDisplay").setModel(oJSONModel);
-
-		},
-
-		initSampleDataModel : function() {
-			var oModel = new JSONModel();
-			var oDateFormat = DateFormat.getDateInstance({source: {pattern: "timestamp"}, pattern: "dd/MM/yyyy"});
-
-			jQuery.ajax(sap.ui.require.toUrl("sap/ui/demo/mock") + "/products.json", {
-				dataType: "json",
-				success: function (oData) {
-					var aTemp1 = [];
-					var aTemp2 = [];
-					var aSuppliersData = [];
-					var aCategoryData = [];
-					for (var i = 0; i < oData.ProductCollection.length; i++) {
-						var oProduct = oData.ProductCollection[i];
-						if (oProduct.SupplierName && jQuery.inArray(oProduct.SupplierName, aTemp1) < 0) {
-							aTemp1.push(oProduct.SupplierName);
-							aSuppliersData.push({Name: oProduct.SupplierName});
-						}
-						if (oProduct.Category && jQuery.inArray(oProduct.Category, aTemp2) < 0) {
-							aTemp2.push(oProduct.Category);
-							aCategoryData.push({Name: oProduct.Category});
-						}
-						oProduct.DeliveryDate = (new Date()).getTime() - (i % 10 * 4 * 24 * 60 * 60 * 1000);
-						oProduct.DeliveryDateStr = oDateFormat.format(new Date(oProduct.DeliveryDate));
-						oProduct.Heavy = oProduct.WeightMeasure > 1000 ? "true" : "false";
-						oProduct.Available = oProduct.Status == "Available" ? true : false;
-					}
-
-					oData.Suppliers = aSuppliersData;
-					oData.Categories = aCategoryData;
-
-					oModel.setData(oData);
-				},
-				error: function () {
-					jQuery.sap.log.error("failed to load json");
-				}
-			});
-
-			return oModel;
-		},
-		
-
-	
 		handleUploadComplete: function(oEvent) {
 			var sResponse = oEvent.getParameter("response");
 			MessageToast.show(sResponse);
@@ -76,8 +27,19 @@ sap.ui.define(
     		
     		var reader = new FileReader();
     		
-    		let convertRowObjectArrayToRecords = function(excelData) {
+    		var convertRowObjectArrayToRecords = function(excelData) {
         	    excelData.Sheet1.splice(0,1);
+        	    
+        	    var items = [];
+        	    
+        	    for( var i = 0; i < excelData.Sheet1.length; i++) {
+        	        items.push({
+        	            "Field1" : excelData.Sheet1[i][0],
+        	            "Field2" : excelData.Sheet1[i][1],
+        	            "Field3" : excelData.Sheet1[i][3],
+        	        });
+        	    }
+        	    
         	    return excelData;
 	        };
     		
