3a8b984baca89817aa2497f020b9dac3ac165a04
S0013963210 <erwin.degula@fonterra.com>
2018-11-12 21:18:23 +0000

    Setting the component to load the manifest.json file

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,31 +2,7 @@ sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
 	"use strict";
 	var Component = UIComponent.extend("com.kalayaan.edegula.ExcelUpload.Component", {
 		metadata: {
-			rootView: {
-				"viewName": "com.kalayaan.edegula.ExcelUpload.view.View",
-				"type": "XML",
-				"async": true
-			},
-			dependencies: {
-				libs: [
-					"sap.ui.unified",
-					"sap.ui.table",
-					"sap.m"
-				]
-			},
-			includes: ["../style.css"],
-			config: {
-				"sample": {
-					"files": [
-						"View.view.xml",
-						"Controller.controller.js"
-					]
-				},
-				"serviceConfig": {
-					"name": "Z_VMI_SRV",
-					"serviceUrl": "/sap/opu/odata/sap/Z_VMI_SRV/"
-				}
-			}
+			manifest: "json"
 		}
 	});
 	return Component;
